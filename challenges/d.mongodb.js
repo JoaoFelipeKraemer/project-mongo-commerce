0f8756d9d5ae555fe037b3106bf22ac6e5573186
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-04-21 17:20:58 -0300

    desafio 15

--- challenges/d.mongodb.js
+++ challenges/d.mongodb.js
@@ -1,4 +1,16 @@
-// use("commerce");
-// db.produtos.find({
-//     valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } },
-//  });
+use("commerce");
+// db.produtos.updateMany(
+//     {},
+//     { $set: { avaliacao: 0 } },
+//  );
+
+// db.produtos.updateMany(
+//     { tags: { $eq: "bovino" } },
+//     { $set: { avaliacao: 5 } },
+//   );
+
+//   db.produtos.updateMany(
+//     { tags: { $eq: "ave" } },
+//     { $set: { avaliacao: 3 } },
+//   );  
+db.produtos.find({});
